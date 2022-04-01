c9c9bc277d4ac9db66fb37933befde94e522a802
SabooBotz <niketutpariani55@gmail.com>
2022-04-01 09:29:33 +0800

    sabobot

---
+++ shop/donasi.js
@@ -0,0 +1,26 @@
+const donasibot = () => {
+return `DONASI BISA VIA E-MONEY / PULSA
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0856 9230 5785
+2. OVO
+	ㅁ Gaada
+3. DANA
+	ㅁ 0821 4520 3493
+
+*📲 PULSA*
+
+1. INDOSAT
+	ㅁ 0856 9230 5785
+2. TELKOMESEL
+    ㅁ 0821 4520 3493
+
+	
+Sebelum Melakukan Pembayaran Harap Hubungi Owner Terlebih Dahulu!
+`
+	}
+
+exports.donasibot = donasibot
+ 
---
+++ shop/sewa.js
@@ -0,0 +1,26 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT VIA GOPAY/DANA
+
+⬡ PER MINGGU = Rp 2.000
+⬡ PER BULAN = Rp 4.000
+⬡ PERMANEN = Rp 6.000
+
+⬣ PRICE SEWABOT VIA PULSA
+
+⬡ PERMANEN = Rp 5.000
+
+
+NOTE : FITUR PREMIUM EROR SETIAP NYALAIN BOT LISTPREM ILANG SEMUA!
+
+
+⬣ MINAT? PM
+⬡ wa.me/6282145203493
+
+
+NOTE : 
+Bot On 24 Jam Jika Tidak Eror `
+	}
+
+exports.pc_sewa = pc_sewa
