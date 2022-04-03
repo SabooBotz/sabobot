0bbd23d976e12ab90a7189c0dff1f07c09335620
SabooBotz <102595659+SabooBotz@users.noreply.github.com>
2022-04-03 18:30:14 +0800

    Add files via upload

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -1,19 +1,9 @@
 // jangan di ubah jika tidak punya group bot, takutnya eror
 const gcbotwa = () =>{
-	return`Silahkan Join Jika Anda Gabut !
+	return`Silahkan Join !
 	
-(Group Bot)
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
-
 (Group Owner Bot)
-1. *JB BT TT  SABO OPBR*
+1. *JB/BT/TT | BY SABO OPBR*
 _https://chat.whatsapp.com/HKNdgk6CV1i06b9ArQ8EjP_
 2. *Group Have Fun*
 _https://chat.whatsapp.com/IsA1bmwV3zH5Lkqg1yZDiL_
